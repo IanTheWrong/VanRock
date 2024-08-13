7874f44f9a29023f53ef05d5f70b14e6acdb7e38
IanTheWrong <ianthewrong@gmail.com>
2024-08-12 20:41:53 -0400

    made it into a stock screener, gonna add gui soon

--- ichimoku.py
+++ ichimoku.py
@@ -10,88 +10,100 @@ with open(filepath, 'r') as file:
 class ichimoku():
     def calculate():
         for ticker in stocks:
-            TenkenSan = 0 #Conversion
+            TenkenSen = 0 #Conversion
             KijunSen = 0 #Base
             SenkouA = 0 #Line A
             SenkouB = 0 #Line B
-            ChikouSan = 0 #Lagging
+            ChikouSen = 0 #Lagging
             tempHigh = 0
-            pastA = 0
+            pastSenkouA = 0
+            pastSenkouB = 0
+            pastTenkenSen = 0
+            PastKijunSen = 0
             with open(f"./StockData/{ticker}.json", 'r') as file:
                 data = json.load(file)
+
+            #TenkenSen
             tempLow = data['results'][0]['l']
             for i in range(0,9):
                 if(tempHigh < data['results'][i]['h']):
                     tempHigh = data['results'][i]['h']
                 if(tempLow > data['results'][i]['l']):
                     tempLow = data['results'][i]['l']
-            TenkenSan = (tempLow+tempHigh)/2
+            TenkenSen = (tempLow+tempHigh)/2
             tempLow = data['results'][0]['l']
             tempHigh = 0
-            for i in range(0,26):
+
+            #PastTenkenSen
+            for i in range(26,36):
                 if(tempHigh < data['results'][i]['h']):
                     tempHigh = data['results'][i]['h']
                 if(tempLow > data['results'][i]['l']):
                     tempLow = data['results'][i]['l']
+            pastTenkenSen = (tempLow+tempHigh)/2
+            tempLow = data['results'][0]['l']
+            tempHigh = 0
+
+            #KijunSen + SenkouA
+            for x in range(0,26):
+                if(tempHigh < data['results'][x]['h']):
+                    tempHigh = data['results'][x]['h']
+                if(tempLow > data['results'][x]['l']):
+                    tempLow = data['results'][x]['l']
             KijunSen = (tempLow+tempHigh)/2
-            SenkouA = (KijunSen+TenkenSan)/2
+            SenkouA = (KijunSen+TenkenSen)/2
             tempLow = data['results'][0]['l']
             tempHigh = 0
-            for i in range(0,52):
-                if(tempHigh < data['results'][i]['h']):
-                    tempHigh = data['results'][i]['h']
-                if(tempLow > data['results'][i]['l']):
-                    tempLow = data['results'][i]['l']
+
+            #PastKijun + PastSenkouA
+            for x in range(26,43):
+                if(tempHigh < data['results'][x]['h']):
+                    tempHigh = data['results'][x]['h']
+                if(tempLow > data['results'][x]['l']):
+                    tempLow = data['results'][x]['l']
+            pastKijunSen = (tempLow+tempHigh)/2
+            pastSenkouA = (pastKijunSen+pastTenkenSen)/2
+            tempLow = data['results'][0]['l']
+            tempHigh = 0
+
+            #SenkouB
+            for f in range(0,52):
+                if(tempHigh < data['results'][f]['h']):
+                    tempHigh = data['results'][f]['h']
+                if(tempLow > data['results'][f]['l']):
+                    tempLow = data['results'][f]['l']
             SenkouB = (tempHigh+tempLow)/2
-            ChikouSan = data['results'][0]['c']
+
+            #PastSenkouB
+            for f in range(26,79):
+                if(tempHigh < data['results'][f]['h']):
+                    tempHigh = data['results'][f]['h']
+                if(tempLow > data['results'][f]['l']):
+                    tempLow = data['results'][f]['l']
+            pastSenkouB = (tempHigh+tempLow)/2
+            ChikouSen = data['results'][0]['c']
             tempLow = 0
             tempHigh = 0
             #buy
-            if(TenkenSan > KijunSen and SenkouA > SenkouB and ChikouSan > SenkouA and vars[f'{ticker}position'] == 1 and vars['Balance'] >= 500):
-                vars['Balance'] -= 500
-                vars[f"{ticker}amt"] = 500/data['results'][0]['c']
-                vars[f'{ticker}bottom'] = SenkouB
-                vars[f'{ticker}position'] = 2
-                vars[f'{ticker}top'] = (ChikouSan-SenkouB)+ChikouSan
-                print("long at", data['results'][0]['c'], f"on {ticker}", "for", vars[f"{ticker}amt"], "shares", vars['Balance'], "remaining")
+            bottom = pastSenkouA
+            top = pastSenkouA
+            if(pastSenkouB < pastSenkouA):
+                bottom = pastSenkouB
+                top = pastSenkouA
+            if(TenkenSen > KijunSen and SenkouA > SenkouB and ChikouSen > pastSenkouA and ChikouSen > pastSenkouB):
+                print("long at", data['results'][0]['c'], f"on {ticker}")
+                with open("results.txt", 'a') as file:
+                    if(ChikouSen * .95 <= top):
+                        file.write(f"----------LONG {ticker} LOSS @ {bottom} LIMIT @ {round((ChikouSen-bottom)*2 + ChikouSen, 2)}\n")
+                    else:
+                        file.write(f"LONG {ticker} LOSS @ {bottom} LIMIT @ {round((ChikouSen-bottom)*2 + ChikouSen, 2)}\n")
             #sell
-            elif(TenkenSan < KijunSen and SenkouA < SenkouB and ChikouSan < SenkouA and vars[f'{ticker}position'] == 1 and vars['Balance'] >= 500):
-                vars['Balance'] -= 500
-                vars[f"{ticker}amt"] = 500/data['results'][0]['c']
-                vars[f'{ticker}top'] = SenkouB
-                vars[f'{ticker}position'] = 0
-                vars[f'{ticker}bottom'] = (ChikouSan-SenkouB)+ChikouSan
-                print("short at", data['results'][0]['c'], f"on {ticker}", "for", vars[f"{ticker}amt"], "shares", vars['Balance'], "remaining")
-            #top sell stop
-            elif(vars[f'{ticker}position'] == 2 and data['results'][0]['c'] > vars[f'{ticker}top'] and vars[f'{ticker}top'] != -1):
-                vars['Balance'] += data['results'][0]['c'] * vars[f"{ticker}amt"]
-                vars[f'{ticker}position'] = 1
-                vars[f'{ticker}top'] = -1
-                vars[f'{ticker}bottom'] = -1
-                vars[f"{ticker}amt"] = 0
-            #bottom sell stop
-            elif(vars[f'{ticker}position'] == 2 and data['results'][0]['c'] < vars[f'{ticker}bottom'] and vars[f'{ticker}top'] != -1):
-                vars['Balance'] += data['results'][0]['c'] * vars[f"{ticker}amt"]
-                vars[f'{ticker}position'] = 1
-                vars[f'{ticker}top'] = -1
-                vars[f'{ticker}bottom'] = -1
-                vars[f"{ticker}amt"] = 0
-            #top sell sell stop
-            if(vars[f'{ticker}position'] == 0 and data['results'][0]['c'] < vars[f'{ticker}top'] and vars[f'{ticker}top'] != -1):
-                vars['Balance'] += data['results'][0]['c'] * vars[f"{ticker}amt"] + vars[f"{ticker}amt"]
-                vars[f'{ticker}position'] = 1
-                vars[f'{ticker}top'] = -1
-                vars[f'{ticker}bottom'] = -1
-                vars[f"{ticker}amt"] = 0
-            elif(vars[f'{ticker}position'] == 0 and data['results'][0]['c'] > vars[f'{ticker}bottom'] and vars[f'{ticker}top'] != -1):
-                vars['Balance'] += vars[f"{ticker}amt"] - (data['results'][0]['c'] * vars[f"{ticker}amt"])
-                vars[f'{ticker}position'] = 1
-                vars[f'{ticker}top'] = -1
-                vars[f'{ticker}bottom'] = -1
-                vars[f"{ticker}amt"] = 0
-
-
-
+            elif(TenkenSen < KijunSen and SenkouA < SenkouB and ChikouSen < pastSenkouA and ChikouSen < pastSenkouB):
+                with open("results.txt", 'a') as file:
+                    if(ChikouSen * 1.05 >= bottom):
+                        file.write(f"----------SHORT {ticker} LOSS @ {top} LIMIT @ {round((ChikouSen-top)*2 + ChikouSen, 2)}\n")
+                    else:
+                        file.write(f"SHORT {ticker} LOSS @ {top} LIMIT @ {round((ChikouSen-top)*2 + ChikouSen, 2)}\n")
             
             
         
--- main.py
+++ main.py
@@ -2,28 +2,11 @@ import shelve
 import os
 import time
 import logging
-exists = False
-# Position 0 is Sell Order
-# Position 1 is Hold Money
-# Position 2 is Buy Order
-filepath = "./tickers.txt"
-stocks = []
-with open(filepath, 'r') as file:
-    content = file.read()
-    stocks = content.splitlines()
-for filename in os.listdir("./"):
-    if filename == "BalanceSheet.dir":
-        exists = True
-        break
-if not exists:
-    vars = shelve.open('BalanceSheet')
-    vars['Balance'] = 500000
-    for ticker in stocks:
-        vars[f"{ticker}position"] = 1
-        vars[f"{ticker}top"] = -1
-        vars[f"{ticker}bottom"] = -1
-        vars[f"{ticker}amt"] = 0
 from data import data
 from ichimoku import ichimoku
-ichimoku.calculate()
+while True:
+    data.get(900)
+    open('results.txt', 'w').close()
+    ichimoku.calculate()
+
 
